4cd40ef3167b82bc3a26895b2b8af8901542138e
Ellaluvscheese <gal21029@byui.edu>
2023-10-11 09:42:42 -0600

    birch commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddToDO></AddToDO>
     </div>
+  );
+}
+
+function AddToDO(){
+  return(
+    <form>
+      <label for="text-form">Add an item to your To-do: </label>
+      <input type="text" id="text-form" />
+      <button>Submit</button>
+    </form>
   );
 }
 
