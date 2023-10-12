02efec3a57aee12a7b70462f71f826ecc97b7f21
Ellaluvscheese <gal21029@byui.edu>
2023-10-12 16:06:59 -0600

    practice / tutorials. fixed button type

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
+import React, { useState } from 'react';
+import Searchbar from './searchbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <AddToDO></AddToDO>
+      <TypedUserItem></TypedUserItem>
+      <Searchbar></Searchbar>
     </div>
   );
 }
 
-function AddToDO(){
+function TypedUserItem(){
+  const [list, setList] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  // const inputChange = (event) => {
+  //   setInputValue(event.target.value);
+  // };
+  const updateList = () => {
+    if (inputValue.trim() !== ''){
+      setList([...list, inputValue]);
+      setInputValue('');
+    };
+  };
   return(
     <form>
       <label for="text-form">Add an item to your To-do: </label>
-      <input type="text" id="text-form" />
-      <button>Submit</button>
+      <input type="text" value={inputValue} onChange={ (e) => setInputValue(e.target.value)} autoComplete='off' />
+      <button type='button' onClick={updateList}>Add item</button>
+
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item} <button type='button'>Done</button></li> 
+        ))}
+      </ul>
     </form>
   );
 }
+
+// function ButtonState(){
+//   const [title, setTitle] = useState("");
+//   const [count, setCount] = useState(0);
+//   const updateTitleClicked = () => {
+//     setTitle("LOL")
+//   };
+//   const updateCounterClicked = () => {
+//     setCount(count + 1)
+//   };
+
+//   return(
+//     <>
+//       <Data title={title} count={count} ></Data>
+//       <button onClick={updateTitleClicked}>Update Title</button>
+//       <button onClick={updateCounterClicked}>Updates Counter</button>
+//     </>
+//   );
+// }
+
+// function Data(props) {
+//   return(
+//     <div>
+//       <p>Title: {props.title} </p>
+//       <p>Count: {props.count}</p>
+//     </div>
+//   );
+// }
+
 
 export default App;
---
+++ src/searchbar.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function Searchbar(props){
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState(0);
+    const [type, setType] = useState('');
+    const [brand, setBrand] = useState('');
+    return(
+        <div>
+            <h2>Search for an item: </h2>
+            <from>
+                <label for='name-field' >Name: </label>
+                <input id="name-field" type='text' value={name} onChange={ (e) => setName(e.target.value)} ></input>
+                <label for='price-field' >Price: </label>
+                <input id="price-field" type='number' value={price} onChange={ (e) => setPrice(e.target.value)} ></input>
+                <label for='type-field' >Type: </label>
+                <input id="type-field" type='text' value={type} onChange={ (e) => setType(e.target.value)} ></input>
+                <label for='brand-field' >Brand: </label>
+                <input id="brand-field" type='text' value={brand} onChange={ (e) => setBrand(e.target.value)} ></input>
+                <button type="button">Search</button>
+            </from>
+        </div>
+    );
+}
+
+export default Searchbar;
